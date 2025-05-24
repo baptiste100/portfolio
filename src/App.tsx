eb463216e4751987d7aedfce67f4b305a04de5df
baptistekieffer <baptiste67.k@gmail.com>
2025-05-24 11:48:16 +0200

    création du projet

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export default function App() {
+    return (
+        <div className="min-h-screen bg-gray-500 text-white flex items-center justify-center text-4xl font-bold">
+            🎉 Tailwind fonctionne parfaitement !
+        </div>
+    );
+}
