65b6c26b6aacd170304dda7fc2ceb0852390d52d
baptistekieffer <baptiste67.k@gmail.com>
2025-07-01 15:52:38 +0200

    feat: Bouton de téléchargement du CV

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,7 +2,7 @@ import githubWhiteIcon from "../assets/icons/github-white.png"
 import githubYellowIcon from "../assets/icons/github-yellow.png"
 import linkedinWhiteIcon from "../assets/icons/linkedin-removebg-preview.png"
 import linkedinYellowIcon from "../assets/icons/linkedin-yellow.png"
-import { Mail, Copy, Check } from "lucide-react";
+import { Mail, Copy, Check, Download } from "lucide-react";
 import { useState } from "react";
 
 export default function Contact () {
@@ -31,6 +31,10 @@ export default function Contact () {
     return (
         <div className="flex flex-col justify-start items-start p-10 gap-3">
             <h1 className="text-2xl mb-1"> Contact </h1>
+            <a href="/cv_baptiste_kieffer.pdf" download className="group transition duration-300  border border-transparent flex p-2 rounded-lg gap-4 font-bold hover:border-customYellow hover:text-customYellow" >
+                <Download/>
+                <p>Télécharger mon CV</p>
+            </a>
             <button onClick={handleCopyClick} className="group transition duration-300  border border-transparent rounded-lg flex p-2 gap-4 hover:text-customYellow hover:border-customYellow hover:cursor-pointer">
                 <Mail className="block group-hover:hidden" />
                 <Mail color="#F5CB5C" className="transition duration-300 hidden group-hover:block" />
