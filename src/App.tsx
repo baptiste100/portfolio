ddda153950f09293914992088119bc2b0d6ef697
baptistekieffer <baptiste67.k@gmail.com>
2025-07-10 11:36:55 +0200

    feat: ajout de Google Analytics pour le suivi du trafic des pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,16 @@ import {Root} from "./root.tsx";
 import About from "./pages/About.tsx";
 import LanguagesList from "./pages/TechnoList.tsx";
 import TechnoList from "./pages/TechnoList.tsx";
+import ReactGA from 'react-ga4' ;
+import {useEffect} from "react";
 
 export default function App() {
+    ReactGA.initialize('G-5MZYK9G3QP') ;
+
+    useEffect(() => {
+        ReactGA.send({ hitType : "pageview", page : window.location.pathname });
+    }, []);
+
     return (
         <div>
             <Element name="home">
